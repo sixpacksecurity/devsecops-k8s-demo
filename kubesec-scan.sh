fc70d444b9e1b0c6cd61287207502f2be2c105f7
sixpacksecurity <91971692+sixpacksecurity@users.noreply.github.com>
2022-08-26 18:55:22 +0530

    Update kubesec-scan.sh
    Fix was to include [0] since there were multiple fields with the key name message/score

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -2,17 +2,18 @@
 
 # using kubesec docker image for scanning
 scan_result=$(docker run -i kubesec/kubesec scan /dev/stdin < k8s_deployment_service.yaml)
-scan_message=$(docker run -i kubesec/kubesec scan /dev/stdin < k8s_deployment_service.yaml | jq .[].message -r)
-scan_score=$(docker run -i kubesec/kubesec scan /dev/stdin < k8s_deployment_service.yaml | jq .[].score)
-
+scan_message=$(docker run -i kubesec/kubesec scan /dev/stdin < k8s_deployment_service.yaml | jq .[0].message -r)
+scan_score=$(docker run -i kubesec/kubesec scan /dev/stdin < k8s_deployment_service.yaml | jq .[0].score)
 	
     # Kubesec scan result processing
     # echo "Scan Score : $scan_score"
 
-if [[ "${scan_score}" -ge 5 ]]; 
+if [ $scan_score -ge 5 ] 
 then
-    echo "Score is $scan_score.\n Kubesec Scan $scan_message"
+    echo "Score is $scan_score"
+    echo "Kubesec Scan $scan_message"
 else
-    echo "Score is $scan_score, which is less than or equal to 5.\n Scanning Kubernetes Resource has Failed"
+    echo "Score is $scan_score, which is less than or equal to 5."
+    echo "Scanning Kubernetes Resource has Failed"
     exit 1;
 fi;
